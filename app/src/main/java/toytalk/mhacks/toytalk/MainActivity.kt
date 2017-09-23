80b71973131a4ece0530448a141416c427c5e431
BinHong Lee <binhong@binhong.me>
2017-09-23 11:41:03 -0400

    Fix attempt 1

--- app/src/main/java/toytalk/mhacks/toytalk/MainActivity.kt
+++ app/src/main/java/toytalk/mhacks/toytalk/MainActivity.kt
@@ -1,13 +1,16 @@
 package toytalk.mhacks.toytalk
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
 
     var lightOne : Switch? = null
 
+    @RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
